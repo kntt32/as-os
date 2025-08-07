bb03069b16b4995e7fb22a3736e529dbc836c355
kntt32 <153593861+kntt32@users.noreply.github.com>
2025-08-07 21:08:14 +0900

    add linker.ld

--- as-build/src/main.rs
+++ as-build/src/main.rs
@@ -10,9 +10,9 @@ use std::{
 fn main() {
     let args: Vec<_> = env::args().collect();
     let release_flag = 2 <= args.len() && args[1] == "release";
-    println!("{}", release_flag);
     
     build_as_boot(release_flag);
+    build_as_kernel(release_flag);
     create_disk(release_flag);
     run_qemu();
 }
@@ -20,6 +20,18 @@ fn main() {
 fn build_as_boot(release_flag: bool) {
     println!("building as-boot ...");
     env::set_current_dir("as-boot").unwrap();
+    let mut command = Command::new("cargo");
+    command.arg("build");
+    if release_flag {
+        command.arg("--release");
+    }
+    command.status().unwrap();
+    env::set_current_dir("../").unwrap();
+}
+
+fn build_as_kernel(release_flag: bool) {
+    println!("building as-kernel ...");
+    env::set_current_dir("as-kernel").unwrap();
     let mut command = Command::new("cargo");
     command.arg("build");
     if release_flag {
@@ -40,6 +52,15 @@ fn create_disk(release_flag: bool) {
     let mut as_boot = File::open(as_boot_path).unwrap();
     let mut as_boot_vec = Vec::new();
     as_boot.read_to_end(&mut as_boot_vec).unwrap();
+
+    let as_kernel_path = if release_flag {
+        Path::new("target/x86_64-unknown-none/release/as-kernel")
+    }else {
+        Path::new("target/x86_64-unknown-none/debug/as-kernel")
+    };
+    let mut as_kernel = File::open(as_kernel_path).unwrap();
+    let mut as_kernel_vec = Vec::new();
+    as_kernel.read_to_end(&mut as_kernel_vec).unwrap();
 
     let disk_path = Path::new("target/disk.img");
     let mut disk = OpenOptions::new()
@@ -65,6 +86,9 @@ fn create_disk(release_flag: bool) {
 
     let mut bootx64 = file_system_efi_boot.create_file("BOOTX64.EFI").unwrap();
     bootx64.write_all(&as_boot_vec).unwrap();
+
+    let mut kernel = file_system_root.create_file("kernel.elf").unwrap();
+    kernel.write_all(&as_kernel_vec).unwrap();
 }
 
 fn run_qemu() {
--- as-kernel/src/main.rs
+++ as-kernel/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
